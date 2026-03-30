1d16dc596dd8d398dcba53f1db1ea531360cfa99
dnjswns98 <tngkrt2004@gmail.com>
2026-03-31 02:41:33 +0900

    feat: User Entity 임시 생성

---
+++ src/main/java/com/fhsh/user/domain/entity/User.java
@@ -0,0 +1,13 @@
+package com.fhsh.user.domain.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
