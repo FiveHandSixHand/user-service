fd99cd39d602d63eee2d2f22677f603e7e3464bd
dnjswns98 <tngkrt2004@gmail.com>
2026-03-30 15:25:02 +0900

    init: 프로젝트 초기 설정

---
+++ src/test/java/com/fhsh/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fhsh.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
