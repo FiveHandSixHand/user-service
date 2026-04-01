86bddb18c4b1fc9578228c9cb03783a7c2b6deb8
dnjswns98 <tngkrt2004@gmail.com>
2026-04-01 11:45:18 +0900

    feat: 유저 상태에 DELETED 추가

--- src/main/java/com/fhsh/daitda/user/domain/enums/UserStatus.java
+++ src/main/java/com/fhsh/daitda/user/domain/enums/UserStatus.java
@@ -8,7 +8,8 @@ import lombok.RequiredArgsConstructor;
 public enum UserStatus {
     PENDING("승인 대기"),
     APPROVED("승인 완료"),
-    REJECTED("가입 거절");
+    REJECTED("가입 거절"),
+    DELETED("탈퇴/삭제");
 
     private final String description;
 }
