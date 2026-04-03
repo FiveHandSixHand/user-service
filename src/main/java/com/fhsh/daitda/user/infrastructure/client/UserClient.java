740c95ee6fe7783c039358e48ed54366f955545d
dnjswns98 <tngkrt2004@gmail.com>
2026-04-04 02:48:20 +0900

    feat: user client 추가

---
+++ src/main/java/com/fhsh/daitda/user/infrastructure/client/UserClient.java
@@ -0,0 +1,15 @@
+package com.fhsh.daitda.user.infrastructure.client;
+
+import com.fhsh.daitda.response.CommonResponse;
+import com.fhsh.daitda.user.presentation.dto.response.UserResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import java.util.UUID;
+
+@FeignClient(name = "user-service")
+public interface UserClient {
+
+    @GetMapping("/internal/v1/users/{userId}")
+    CommonResponse<UserResponse> getUserById(@PathVariable("userId") UUID userId);
+}
