30b480f7b3c9b382cf421917075ba69c4b93f857
dnjswns98 <tngkrt2004@gmail.com>
2026-04-06 03:56:38 +0900

    feat: keycloak config 추가

---
+++ src/main/java/com/fhsh/daitda/user/config/KeycloakConfig.java
@@ -0,0 +1,38 @@
+package com.fhsh.daitda.user.config;
+
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KeycloakConfig {
+
+    @Value("${keycloak.server-url}")
+    private String serverUrl;
+
+    @Value("${keycloak.realm}")
+    private String realm;
+
+    @Value("${keycloak.admin.username}")
+    private String adminUsername;
+
+    @Value("${keycloak.admin.password}")
+    private String adminPassword;
+
+    @Value("${keycloak.admin.client-id:admin-cli}")
+    private String adminClientId;
+
+    @Bean
+    public Keycloak keycloak() {
+        return KeycloakBuilder.builder()
+                .serverUrl(serverUrl)
+                .realm(realm) // 보통 관리를 위해 master realm을 사용하거나 지정된 realm을 사용
+                .grantType(adminPassword)
+                .clientId(adminClientId)
+                .username(adminUsername)
+                .password(adminPassword)
+                .build();
+    }
+}
