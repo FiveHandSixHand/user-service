4e03eef0142f846c4eda1fe8a67ca98637096410
dnjswns98 <tngkrt2004@gmail.com>
2026-04-06 12:59:11 +0900

    refactor: toString() 비밀번호 마스킹

--- src/main/java/com/fhsh/daitda/user/application/command/UserSignupCommand.java
+++ src/main/java/com/fhsh/daitda/user/application/command/UserSignupCommand.java
@@ -12,4 +12,16 @@ public record UserSignupCommand(
         UUID hubId,
         UUID companyId
 ) {
+    @Override
+    public String toString() {
+        return "UserSignupCommand[" +
+                "email='" + email + '\'' +
+                ", password='****'" + // 비밀번호 마스킹 처리
+                ", name='" + name + '\'' +
+                ", role=" + role +
+                ", slackUserId='" + slackUserId + '\'' +
+                ", hubId=" + hubId +
+                ", companyId=" + companyId +
+                ']';
+    }
 }
--- src/main/java/com/fhsh/daitda/user/presentation/dto/request/UserSignupRequest.java
+++ src/main/java/com/fhsh/daitda/user/presentation/dto/request/UserSignupRequest.java
@@ -12,4 +12,16 @@ public record UserSignupRequest(
         UUID hubId,
         UUID companyId
 ) {
+    @Override
+    public String toString() {
+        return "UserSignupRequest[" +
+                "email='" + email + '\'' +
+                ", password='****'" + // 비밀번호 마스킹 처리
+                ", name='" + name + '\'' +
+                ", role=" + role +
+                ", slackUserId='" + slackUserId + '\'' +
+                ", hubId=" + hubId +
+                ", companyId=" + companyId +
+                ']';
+    }
 }
