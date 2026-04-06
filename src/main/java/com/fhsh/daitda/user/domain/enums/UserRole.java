44fd21bb018fa30ac988476ae85911af94e3df76
dnjswns98 <tngkrt2004@gmail.com>
2026-04-07 04:22:34 +0900

    chore: ADMIN으로 수정

--- src/main/java/com/fhsh/daitda/user/domain/enums/UserRole.java
+++ src/main/java/com/fhsh/daitda/user/domain/enums/UserRole.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum UserRole {
-    MASTER("마스터 관리자"),
+    ADMIN("마스터 관리자"),
     HUB_ADMIN("허브 관리자"),
     DELIVERY("배송 담당자"),
     COMPANY("업체 담당자");
