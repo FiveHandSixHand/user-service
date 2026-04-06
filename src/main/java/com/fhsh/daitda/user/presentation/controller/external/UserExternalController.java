23066694a0649f788f2f3448c6bc4019957798f5
dnjswns98 <tngkrt2004@gmail.com>
2026-04-07 02:01:50 +0900

    feat: 회원가입 서비스 연결

--- src/main/java/com/fhsh/daitda/user/presentation/controller/external/UserExternalController.java
+++ src/main/java/com/fhsh/daitda/user/presentation/controller/external/UserExternalController.java
@@ -31,9 +31,7 @@ public class UserExternalController {
                 request.hubId(),
                 request.companyId()
         );
-
-
-
+        userCommandService.signup(command);
         return CommonResponse.success();
     }
 }
