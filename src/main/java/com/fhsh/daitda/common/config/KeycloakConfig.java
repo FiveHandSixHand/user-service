8e763f9e02bf2638fd67368e3e4820e476130f49
dnjswns98 <tngkrt2004@gmail.com>
2026-04-07 03:04:57 +0900

    rename: 패키지 경로 수정

--- src/main/java/com/fhsh/daitda/common/config/KeycloakConfig.java
+++ src/main/java/com/fhsh/daitda/common/config/KeycloakConfig.java
@@ -1,4 +1,4 @@
-package com.fhsh.daitda.user.config;
+package com.fhsh.daitda.common.config;
 
 import org.keycloak.OAuth2Constants;
 import org.keycloak.admin.client.Keycloak;
--- src/main/java/com/fhsh/daitda/common/config/RedisConfig.java
+++ src/main/java/com/fhsh/daitda/common/config/RedisConfig.java
@@ -1,4 +1,4 @@
-package com.fhsh.daitda.user.config;
+package com.fhsh.daitda.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
