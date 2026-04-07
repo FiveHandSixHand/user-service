743dd630bc4de91a338239e00b17bfed22757f7f
dnjswns98 <tngkrt2004@gmail.com>
2026-04-07 12:51:43 +0900

    feat: 유저 목록 조회 로직 작성

--- src/main/java/com/fhsh/daitda/user/application/service/query/UserQueryService.java
+++ src/main/java/com/fhsh/daitda/user/application/service/query/UserQueryService.java
@@ -1,10 +1,14 @@
 package com.fhsh.daitda.user.application.service.query;
 
 import com.fhsh.daitda.exception.BusinessException;
+import com.fhsh.daitda.user.application.command.UserSearchCommand;
 import com.fhsh.daitda.user.application.result.UserQueryResult;
 import com.fhsh.daitda.user.domain.exception.UserErrorCode;
+import com.fhsh.daitda.user.domain.repository.UserCustomRepository;
 import com.fhsh.daitda.user.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.UUID;
@@ -14,10 +18,16 @@ import java.util.UUID;
 @Transactional(readOnly = true)
 public class UserQueryService {
     private final UserRepository userRepository;
+    private final UserCustomRepository userCustomRepository;
 
     public UserQueryResult getUserById(UUID userId) {
         return userRepository.findById(userId)
                 .map(UserQueryResult::from)
                 .orElseThrow(() -> new BusinessException(UserErrorCode.USER_NOT_FOUND));
+    }
+
+    public Page<UserQueryResult> getUsers(UserSearchCommand command, Pageable pageable) {
+        return userCustomRepository.findAll(command, pageable)
+                .map(UserQueryResult::from);
     }
 }
